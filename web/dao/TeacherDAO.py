c63a6d76f1600be36eeb1c410d4cd3843a4cbd8e
zhangshuo1996 <18796360983zs@gmail.com>
2020-09-20 15:57:44 +0800

    整合之前的提交

--- web/dao/TeacherDAO.py
+++ web/dao/TeacherDAO.py
@@ -1,45 +1,45 @@
-"""
-根据教师id获取教师的基本信息以及成果信息
-2020.3.6
-by zhang
-"""
-from web.utils import db
-
-
-def get_teacher_info(teacher_id):
-    """
-
-    :param teacher_id:
-    :return:
-    """
-    # 查询基本信息
-    sql1 = "SELECT ID id, `NAME` name, TITLE title, EDUEXP edu_exp, EMAIL email, HOMEPAGE homepage, BIRTHYEAR birthyear " \
-           "from es_teacher where ID = " + str(teacher_id)
-    teacher_info = db.execute(sql1)
-    if len(teacher_info) > 0:
-        teacher_info = teacher_info[0]
-
-    sql4 = "SELECT `NAME` school_name from es_school " \
-            "where ID = (SELECT SCHOOL_ID from es_teacher where ID = " + str(teacher_id) + ")"
-    school = db.execute(sql4)[0]["school_name"]
-
-    sql5 = "SELECT `NAME` institution_name from es_institution " \
-            "where ID = (SELECT INSTITUTION_ID from es_teacher where ID = " + str(teacher_id) + ")"
-    institution = db.execute(sql5)[0]["institution_name"]
-
-    # 查询论文
-    sql2 = "SELECT id, name paper_name, org, `year`, cited_num " \
-            "from paper " \
-            "where id in ( SELECT paper_id from teacher_paper where teacher_id = " + str(teacher_id) + ")"
-    paper_info = db.execute(sql2)
-
-    # 查询专利信息
-    sql3 = "SELECT id, title, publication_year year, applicant, inventor " \
-            "from patent " \
-            "where id in ( SELECT patent_id from teacher_patent2 where teacher_id = " + str(teacher_id) + ")"
-    patent_info = db.execute(sql3)
-
-    teacher_info["school"] = school
-    teacher_info["institution"] = institution
-
-    return teacher_info, paper_info, patent_info
+# """
+# 根据教师id获取教师的基本信息以及成果信息
+# 2020.3.6
+# by zhang
+# """
+# from web.utils import db
+#
+#
+# def get_teacher_info(teacher_id):
+#     """
+#
+#     :param teacher_id:
+#     :return:
+#     """
+#     # 查询基本信息
+#     sql1 = "SELECT ID id, `NAME` name, TITLE title, EDUEXP edu_exp, EMAIL email, HOMEPAGE homepage, BIRTHYEAR birthyear " \
+#            "from es_teacher where ID = " + str(teacher_id)
+#     teacher_info = db.execute(sql1)
+#     if len(teacher_info) > 0:
+#         teacher_info = teacher_info[0]
+#
+#     sql4 = "SELECT `NAME` school_name from es_school " \
+#             "where ID = (SELECT SCHOOL_ID from es_teacher where ID = " + str(teacher_id) + ")"
+#     school = db.execute(sql4)[0]["school_name"]
+#
+#     sql5 = "SELECT `NAME` institution_name from es_institution " \
+#             "where ID = (SELECT INSTITUTION_ID from es_teacher where ID = " + str(teacher_id) + ")"
+#     institution = db.execute(sql5)[0]["institution_name"]
+#
+#     # 查询论文
+#     sql2 = "SELECT id, name paper_name, org, `year`, cited_num " \
+#             "from paper " \
+#             "where id in ( SELECT paper_id from teacher_paper where teacher_id = " + str(teacher_id) + ")"
+#     paper_info = db.execute(sql2)
+#
+#     # 查询专利信息
+#     sql3 = "SELECT id, title, publication_year year, applicant, inventor " \
+#             "from patent " \
+#             "where id in ( SELECT patent_id from teacher_patent2 where teacher_id = " + str(teacher_id) + ")"
+#     patent_info = db.execute(sql3)
+#
+#     teacher_info["school"] = school
+#     teacher_info["institution"] = institution
+#
+#     return teacher_info, paper_info, patent_info
--- web/service/TeacherService.py
+++ web/service/TeacherService.py
@@ -1,26 +1,26 @@
-"""
-获取专家基本信息以及成果信息
-"""
-from web.utils import db
-import sys
-import pprint
-sys.path.append("..")
-from web.dao.TeacherDAO import *
-
-
-def get_info(teacher_id):
-    """
-
-    :param teacher_id:
-    :return:
-    """
-    teacher_info, paper_info, patent_info = get_teacher_info(teacher_id)
-    teacher_info["papers"] = paper_info
-
-    for d in patent_info:
-        d["inventor"] = eval(d["inventor"])
-        d["applicant"] = eval(d["applicant"])
-
-    teacher_info["patents"] = patent_info
-    # print(pprint.pformat(teacher_info))
-    return teacher_info
+# """
+# 获取专家基本信息以及成果信息
+# """
+# from web.utils import db
+# import sys
+# import pprint
+# sys.path.append("..")
+# from web.dao.TeacherDAO import *
+#
+#
+# def get_info(teacher_id):
+#     """
+#
+#     :param teacher_id:
+#     :return:
+#     """
+#     teacher_info, paper_info, patent_info = get_teacher_info(teacher_id)
+#     teacher_info["papers"] = paper_info
+#
+#     for d in patent_info:
+#         d["inventor"] = eval(d["inventor"])
+#         d["applicant"] = eval(d["applicant"])
+#
+#     teacher_info["patents"] = patent_info
+#     # print(pprint.pformat(teacher_info))
+#     return teacher_info
